cf112b8b6ee9c778dd983a3cc0cb6783dc5b2381
Andrei Shevyriaev <andrei.shevyriaev@gmail.com>
2018-08-30 02:51:59 +0300

    Android-8. Update API URL in "APIService" after Close "SettingsActivity".

--- client/mobile/app/src/main/java/com/example/mike/momonga/ui/settings/SettingsActivity.java
+++ client/mobile/app/src/main/java/com/example/mike/momonga/ui/settings/SettingsActivity.java
@@ -1,9 +1,12 @@
 package com.example.mike.momonga.ui.settings;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
 
 import com.example.mike.momonga.R;
+import com.example.mike.momonga.api.APIService;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -11,5 +14,16 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String            api_url     = preferences.getString("api_url",null);
+        if(api_url == null) {
+            api_url = getResources().getString(R.string.default_api_url);
+        }
+        APIService.setBaseURL(api_url);
     }
 }
