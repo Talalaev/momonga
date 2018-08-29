ced735b1fe80f7fc8f72455a6988565063dbd913
Andrei Shevyriaev <andrei.shevyriaev@gmail.com>
2018-08-30 00:48:45 +0300

    Android-8. Add method to set connection timeout.

--- client/mobile/app/src/main/java/com/example/mike/momonga/api/APIService.java
+++ client/mobile/app/src/main/java/com/example/mike/momonga/api/APIService.java
@@ -1,24 +1,37 @@
 package com.example.mike.momonga.api;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIService {
 
-    private static APIInterface sInstance   = null;
-    private static String       sBaseURL    = null;
+    private static APIInterface sInstance           = null;
+    private static String       sBaseURL            = null;
+    private static int          sConnectionTimeout  = 5;
 
     public static void setBaseURL(String pBaseURL) {
         sBaseURL    = pBaseURL;
         sInstance   = null;
     }
 
+    public static void setConnectionTimeout(int pConnectionTimeout) {
+        sConnectionTimeout = pConnectionTimeout;
+        sInstance = null;
+    }
+
     public static APIInterface getInstance() {
         if (sInstance == null) {
-             Retrofit retrofit = new retrofit2.Retrofit.Builder()
-                    .baseUrl(sBaseURL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+            OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(sConnectionTimeout  , TimeUnit.SECONDS)
+                .build();
+            Retrofit retrofit = new retrofit2.Retrofit.Builder()
+                .client(client)
+                .baseUrl(sBaseURL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
             sInstance = retrofit.create(APIInterface.class);
         }
         return sInstance;
