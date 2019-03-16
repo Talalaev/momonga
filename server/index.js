c8e75e4bb24ac7020097ea85255f4fd7b5aceaf8
mike <ellokopoko@gmail.com>
2019-03-16 21:54:02 +0300

    .

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 const app = require('./app');
 const server = app.listen(8888);
 
+app.listen(app.get('port'), function() {
+  if (process.env.DYNO) {
+    console.log('This is on Heroku..!!');
+    fs.openSync('/tmp/app-initialized', 'w');
+  }
+  console.log('Node app is running on port', app.get('port'));
+});
+
 console.log(`start!`);
 console.log(`app: http://localhost:3000`);
 console.log(`doc: http://localhost:3000/public/api-docs`);
