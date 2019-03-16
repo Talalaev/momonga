bb96c4b7a722819dab7712c0813dab6158456ae4
mike <ellokopoko@gmail.com>
2019-03-17 00:06:00 +0300

    .

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
+const fs = require('fs');
 const app = require('./app');
 const server = app.listen(8888);
 
 console.log(`start!`);
 console.log(`app: http://localhost:3000`);
 console.log(`doc: http://localhost:3000/public/api-docs`);
+
+if (process.env.DYNO) {
+  console.log('This is on Heroku..!!');
+  fs.openSync('/tmp/app-initialized', 'w');
+}
 
 module.exports = server;
--- server/middlewares/allowHosts.js
+++ server/middlewares/allowHosts.js
@@ -1,15 +1,5 @@
-var fs = require('fs');
-
 function allowHosts() {
     return (ctx, next) => {
-
-      if (process.env.DYNO) {
-        console.log('This is on Heroku..!!');
-        fs.openSync('/tmp/app-initialized', 'w');
-      }
-      console.log('Node app is running on port', 8888);
-
-
         ctx.response.set("Access-Control-Allow-Origin", "http://localhost:3000");
         ctx.response.set("Access-Control-Allow-Origin", "http://localhost:4200");
         ctx.response.set("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
