56f3e3ad4e08432093e8b87e34c3cbae59551ccc
mike <ellokopoko@gmail.com>
2019-03-16 21:37:50 +0300

    .

--- server/middlewares/returnApp.js
+++ server/middlewares/returnApp.js
@@ -1,18 +1,18 @@
-const User = require('../models/user');
+// const User = require('../models/user');
 
 function returnApp() {
     return async (ctx, next) => {
-        let user = null;
-
-        if (ctx.session.passport) {
-            user = await User.findOne({where: {id: ctx.session.passport.user}});
-
-            if (user) {
-                console.log('\x1b[32m%s\x1b[0m', "---------\nIs the user authorized? -> true\n---------");
-            } else {
-                console.log('\x1b[33m%s\x1b[0m:', `---------\nIs the user authorized? -> false\n---------`);
-            }
-        }
+        // let user = null;
+        //
+        // if (ctx.session.passport) {
+        //     user = await User.findOne({where: {id: ctx.session.passport.user}});
+        //
+        //     if (user) {
+        //         console.log('\x1b[32m%s\x1b[0m', "---------\nIs the user authorized? -> true\n---------");
+        //     } else {
+        //         console.log('\x1b[33m%s\x1b[0m:', `---------\nIs the user authorized? -> false\n---------`);
+        //     }
+        // }
 
         try {
             await ctx.render('client/pwa/dist/index.html');
