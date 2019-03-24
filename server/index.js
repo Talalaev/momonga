24ede2ad6257bec76c920751b8fee62e37c3dd10
mike <ellokopoko@gmail.com>
2019-03-24 19:11:16 +0300

    .

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ console.log(`start!`);
 console.log(`app: http://localhost:3000`);
 console.log(`doc: http://localhost:3000/public/api-docs`);
 
+
 if (process.env.DYNO) {
   console.log('This is on Heroku..!!');
   fs.openSync('/tmp/app-initialized', 'w');
